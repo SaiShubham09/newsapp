7216b1cfa160d04977cbf4cc5300e7db2622d074
Sai Shubham Pasayat <65237435+SaiShubham09@users.noreply.github.com>
2022-01-20 20:20:36 +0530

    top loader bar added

--- src/components/News.js
+++ src/components/News.js
@@ -33,17 +33,21 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=99e253f16b1542b8b48132fa07f7a100&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
+        this.props.setProgress(60);
         // console.log(parsedData);
+
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         });
-
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,20 +3,25 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let {title,description,imageUrl,newsUrl,author,date, source} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card" >
-                    <span className='position-absolute top-0 translate-middle badge rounded-pill bg-dark' style={{left: '90%', zIndex: '1'}}> {source}
-
-                    </span>
-                    <img src={imageUrl?imageUrl:"https://www.bollyinside.com/wp-content/uploads/2021/12/Elon-Musk-rejects-mounting-criticism-that-his-satellites-are-obstructing.png"} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <p className="card-text"><small className="text-muted">Published by {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
-                            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
-                        </div>
+                    <div style={{
+                        display: 'flex',
+                        justifyConetent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }}>
+                        <span className=' badge rounded-pill bg-dark'> {source}</span>
+                    </div>
+                    <img src={imageUrl ? imageUrl : "https://www.bollyinside.com/wp-content/uploads/2021/12/Elon-Musk-rejects-mounting-criticism-that-his-satellites-are-obstructing.png"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">Published by {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
                 </div>
             </div>
         )
